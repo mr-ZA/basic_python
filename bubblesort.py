6f02f274c0fca37f151537b82b6d256e929c3d4e
mr-ZA <29811573+mr-ZA@users.noreply.github.com>
2019-07-19 00:46:37 +0300

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def bubblesort(lst):
+def desc_bubblesort(lst):
     for x in range (len (lst)):               # 0 - 4
         for j in range (x + 1, len(lst)):     # 1 - 4
             if lst[j] > lst[x]:
@@ -9,11 +9,23 @@ def bubblesort(lst):
                 lst[x] = temp
     return lst
 
-def main():
-    mass_1 = [i for i in range (0, 5)]      #generator of the list
-    print(type(mass_1))
+def asc_bubblesort(lst):
+    for x in range (len (lst)):
+        for z in range (x+1, len(lst)):
+            if lst[z] < lst[x]:
+                temp = lst[x]
+                lst[x] = lst[z]
+                lst[z] = temp
+    return lst
 
-    print (bubblesort(mass_1))
+def main():
+    mass_1 = [i for i in range (0, 5)]      # generator of the list desc
+    mass_2 = list (reversed(mass_1))        # for ascending method [4, 3, 2 ...]
+    #print(type(mass_1))
+    #print(type(mass_2))
+
+    print (desc_bubblesort(mass_1))
+    print(asc_bubblesort(mass_2))
 
 if __name__ == '__main__':
     main()
