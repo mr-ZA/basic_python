c2e19b5fe7efdc8945c652e8cd86574d24c8867b
mr-ZA <29811573+mr-ZA@users.noreply.github.com>
2019-08-24 16:35:21 -0800

    Create args_kwargs.py

---
+++ args_kwargs.py
@@ -0,0 +1,38 @@
+def myFunc_args (*args):
+    zex = 1
+
+    for a in args:
+        zex *= a
+
+    print (zex)
+
+def myFunc_kwargs (**kwargs):
+    if not kwargs:
+        print (("Функция <{}()>, {}: {}\n").format("myFunc_kwargs", "принимает словарь", kwargs))
+    else:
+        for k, v in kwargs.items():
+            print (k, v)
+
+        print("\n")
+        print ("{} {}".format("Первый элемент словаря: ", kwargs['kw_1']))
+
+        print("\n")
+        print (kwargs.keys())
+        print(kwargs.values())
+
+        print("\nЗамена первого элемента словаря:")
+        kwargs["kw_1"] = "Elephant"
+        for k, v in kwargs.items():
+            print (k, v)
+
+def main():
+    print ("Функция myFunc_args: ")
+    myFunc_args (3, 4, 10)
+    print("\n")
+
+    myFunc_kwargs()
+
+    myFunc_kwargs(kw_1 = "Shark", kw_2 = "Lion")
+
+if __name__ == '__main__':
+    main()
