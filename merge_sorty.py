03af61c839d95c74ad6b225e6fccf05814b34e11
etimerbulatov <etimerbulatov@national-platform.ru>
2019-09-23 11:49:53 +0300

    23_09

--- merge_sorty.py
+++ merge_sorty.py
@@ -7,11 +7,13 @@ def merge(left_list, right_list):
 
     for _ in range(left_list_length + right_list_length):
         if left_list_index < left_list_length and right_list_index < right_list_length:
+
             # Мы проверяем, какое значение с начала каждого списка меньше
             # Если элемент в начале левого списка меньше, добавляем его в отсортированный список
             if left_list[left_list_index] <= right_list[right_list_index]:
                 sorted_list.append(left_list[left_list_index])
                 left_list_index += 1
+
             # Если элемент в начале правого списка меньше, добавляем его в отсортированный список
             else:
                 sorted_list.append(right_list[right_list_index])
@@ -21,6 +23,7 @@ def merge(left_list, right_list):
         elif left_list_index == left_list_length:
             sorted_list.append(right_list[right_list_index])
             right_list_index += 1
+
         # Если мы достигли конца правого списка, добавляем элементы из левого списка
         elif right_list_index == right_list_length:
             sorted_list.append(left_list[left_list_index])
---
+++ parser_example/parse_site.py
@@ -0,0 +1,42 @@
+import selenium
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+def wr_cred(driver, login = "", passw = ""):
+    elem_login = driver.find_element_by_xpath("//*[@id=\"login_field\"]")  # fields search
+    elem_passwd = driver.find_element_by_xpath("//*[@id=\"password\"]")
+
+    action = ActionChains(driver)
+    action.move_to_element(elem_login)
+    action.perform()
+
+    elem_login.clear()  # for next iter enter
+    elem_login.send_keys (login)
+    elem_login.send_keys (Keys.TAB)
+
+    elem_passwd.send_keys (passw)
+    elem_passwd.send_keys (Keys.RETURN)
+
+def main():
+    driver = selenium.webdriver.Chrome (r'drivers\chromedriver.exe')
+    driver.get ('https://github.com/login')
+    wr_cred (driver, "", "")    # login, passw params
+    flag = True     # one time at least
+
+    while flag:
+        try:
+            flag = driver.find_element_by_xpath("//*[@id=\"js-flash-container\"]/div")
+        except:
+            flag = False
+            continue
+
+        print("Wrong credentials, try again")
+        login = input("Enter login: \n")
+        passw = input("Enter password: \n")
+        wr_cred(driver, login, passw)
+
+    print("Authorization success")
+    driver.close()
+
+if __name__ == '__main__':
+    main()
